7b4c4dc6c89697e92564608396f2331f231558a7
xuqr <1163990163@qq.com>
2020-07-18 01:02:20 +0800

    #调整爆炸位置

--- src/cn/xqrcloud/tank/Tank.java
+++ src/cn/xqrcloud/tank/Tank.java
@@ -112,7 +112,14 @@ public class Tank {
                 break;
         }
 
-        if(random.nextInt(10)>5) this.fire();
+        if(this.group==Group.BAD&&random.nextInt(100)>90) this.fire();
+        if(this.group==Group.BAD&&random.nextInt(100)>95)
+        randomDir();
+    }
+
+    private void randomDir() {
+
+        this.dir = Dir.values()[random.nextInt(4)];
     }
 
     public void fire() {
